e6eb93c2962962f729657ea13f50a75187c93ad9
yogjin <youngjin988@gmail.com>
2023-07-09 21:58:55 +0900

    feat: `UX-technique` 페이지 생성

--- src/router/routes/parent/rootRoutes.tsx
+++ src/router/routes/parent/rootRoutes.tsx
@@ -5,6 +5,7 @@ import { Outlet } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from '../../../pages/Home';
 import Layout from '../../../components/Layout/Layout';
+import { UXChildren } from '../children/UXChildren';
 
 const queryClient = new QueryClient();
 
@@ -45,5 +46,19 @@ export const rootRoutes: Route[] = [
     ),
     children: tanstackQueryChildren,
     title: 'tanstack-query',
+  },
+  {
+    path: '/ux-technique',
+    element: (
+      <Layout
+        title="ux-technique"
+        routes={UXChildren}
+        render={<Outlet />}
+        color="#EF4444"
+        backgroundColor="#1E1E1E"
+      />
+    ),
+    children: UXChildren,
+    title: 'UX-technique',
   },
 ];
